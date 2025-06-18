525e43fb99941358ff147c8fb30cf3673d96c7fa
dahoon06 <dahoon226@naver.com>
2025-06-18 18:21:28 +0900

    init

--- api/index.ts
+++ api/index.ts
@@ -2,9 +2,12 @@ import express, { Request, Response } from "express";
 import serverless from "serverless-http";
 
 const app = express();
+const router = express.Router();
 
 app.get("/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello from TypeScript + Express + Vercel!" });
 });
+
+app.use("/api", router);
 
 export default serverless(app);
