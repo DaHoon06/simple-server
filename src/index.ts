2a3b9f0b46e0b95a316253fae9a8ea94993ec843
dahoon06 <dahoon226@naver.com>
2025-06-18 17:47:51 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3333;
+
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript!");
+});
+
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행중입니다.`);
+});
